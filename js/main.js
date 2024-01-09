e88f32e79ba9a930fc05da26f82250840d4db782
CRemp <69777888+clyderemp@users.noreply.github.com>
2024-01-08 23:22:02 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ $(function(){
           // User is signed in, see docs for a list of available properties
           // https://firebase.google.com/docs/reference/js/auth.user
           const uid = user.uid;
-          console.log('logged in: '+uid);
+          console.log('logged in user: '+uid);
                 window.location.href = "/dashboard/user/home/index.html";
 
             });
